fd0548b93581e2ce420145b56f5cfa9317a2cfab
Arland Porter <arnavpon@gmail.com>
2017-11-28 17:17:07 -0500

    integrating with mongo

--- patient/patient.py
+++ patient/patient.py
@@ -4,9 +4,9 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 from scope import Scope
 
-# client = MongoClient('mongodb://localhost:27017/')  # connect to mongoDB @ default port of localhost
-# db = client.patients  # specify the DB to use ('patients')
-client = MongoClient("mongodb://arnavpon:warhammeR10@mongodb/")  # /patients ** connect to MongoDB
+client = MongoClient('mongodb://localhost:27017/')  # connect to mongoDB @ default port of localhost
+
+#client = MongoClient("mongodb://arnavpon:warhammeR10@mongodb/")  # /patients ** connect to MongoDB
 db = client.patients  # specify the DB to access (patients)
 print("Connected to db: {} with collections = {}".format(db, db.collection_names()))
 
